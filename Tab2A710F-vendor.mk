aecf0645031148cd2d37735201318108791e1c48
Tirth Patel <tirthpatel49@gmail.com>
2016-10-10 20:43:23 +0530

    Initial Commit

---
+++ Tab2A710F-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/Tab2A710F/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/Tab2A710F/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/Tab2A710F/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/Tab2A710F/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/Tab2A710F/proprietary/vendor,system/vendor)
